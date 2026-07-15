c781566a4cdb82fddd9e6fc37e0a54e89cdac703
112434038-sudo <112434038@kanchiuniv.ac.in>
2026-07-14 20:39:36 -0700

    Add files via upload

---
+++ reo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Client Form</title>
+</head>
+<body>
+
+<h2>Submit Form</h2>
+
+<form action="process.php" method="post">
+    <label>Name:</label>
+    <input type="text" name="name" required>
+    <br><br>
+
+    <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
